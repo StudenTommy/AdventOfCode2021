d457e5133c2fb93d335cdfff2897405adbaaaaa0
ThinkTommy <thinktommy@gmx.de>
2022-03-18 21:23:30 +0100

    Finished Day2 challenges

--- Day2/Part2.py
+++ Day2/Part2.py
@@ -2,15 +2,18 @@ file = open("input Day2", "r")
 
 forwardCount = 0
 depthCount = 0
+aimCount = 0
 
 for line in file:
     if "forward" in line:
         forwardCount += int(line.split()[1])
+        depthCount += aimCount * int(line.split()[1])
     elif "down" in line:
-        depthCount += int(line.split()[1])
+        aimCount += int(line.split()[1])
     else:
-        depthCount -= int(line.split()[1])
+        aimCount -= int(line.split()[1])
 
 print("forwardCount: " + str(forwardCount))
 print("depthCount: " + str(depthCount))
+print("aimCount: " + str(aimCount))
 print(forwardCount * depthCount)
